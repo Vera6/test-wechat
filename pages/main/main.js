39e2636cd147ed52ab2023d1d56be95afd040c0e
za-huangwei001 <huangwei001@zhongan.io>
2019-08-12 11:21:54 +0800

    'main构建'

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,66 +1,30 @@
-// pages/main/main.js
+const mainData = require('../../data/posts-data.js')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      mainList: mainData.postList
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 轮播图点击
+  onSwiperTap: function (event) {
+    // target指的是当前点击的组件
+    const mainId = event.target.dataset.postid
+    wx.navigateTo({
+      url: 'main-detail/main-detail?id=' + mainId
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 列表图点击
+  onMainTap: function (event) {
+    // currentTarget 指的是事件捕获的组件
+    const mainId = event.currentTarget.dataset.postid
+    wx.navigateTo({
+      url: 'main-detail/main-detail?id=' + mainId
+    })
   }
 })
